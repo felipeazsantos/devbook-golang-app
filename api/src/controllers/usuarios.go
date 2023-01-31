13199f6c74b6259fcbe87a94b41f21d0782d82c7
Felipe Azevedo dos Santos <felipe.azevedo@cws.digital>
2023-01-31 19:40:04 -0300

    Pacote Respostas

--- api/src/controllers/usuarios.go
+++ api/src/controllers/usuarios.go
@@ -4,10 +4,9 @@ import (
 	"DevBook/api/src/banco"
 	"DevBook/api/src/modelos"
 	"DevBook/api/src/repositorios"
+	"DevBook/api/src/respostas"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 )
 
@@ -15,26 +14,33 @@ import (
 func CriarUsuario(w http.ResponseWriter, r *http.Request) {
 	corpoRequisicao, erro := ioutil.ReadAll(r.Body)
 	if erro != nil {
-		log.Fatal(erro)
+		respostas.Erro(w, http.StatusUnprocessableEntity, erro)
+		return
 	}
 
 	var usuario modelos.Usuario
 
 	if erro = json.Unmarshal(corpoRequisicao, &usuario); erro != nil {
-		log.Fatal(erro)
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
 	}
 
 	db, erro := banco.Conectar()
 	if erro != nil {
-		log.Fatal(erro)
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
 	}
+	defer db.Close()
 
 	repositorio := repositorios.NovoRepositorioDeUsuario(db)
-	usuarioId, erro := repositorio.Criar(usuario)
+	usuario.ID , erro = repositorio.Criar(usuario)
 	if erro != nil {
-		log.Fatal(erro)
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
 	}
-	w.Write([]byte(fmt.Sprintf("Usuário inserido com sucesso! Id: %d", usuarioId)))
+
+	respostas.JSON(w, http.StatusCreated, usuario)
+
 }
 
 // BuscarUsuarios retorna todos os usuário do banco de dados
