3dc2413fb62a5ce1e67becd22ed68c4140af5d0b
Felipe Azevedo dos Santos <felipe.azevedo@cws.digital>
2023-02-02 14:06:08 -0300

    Login

---
+++ api/src/controllers/login.go
@@ -0,0 +1,48 @@
+package controllers
+
+import (
+	"DevBook/api/seguranca"
+	"DevBook/api/src/banco"
+	"DevBook/api/src/modelos"
+	"DevBook/api/src/repositorios"
+	"DevBook/api/src/respostas"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+//Login é responsável por autenticar um usuáiro na API
+func Login(w http.ResponseWriter, r *http.Request) {
+	corpoRequisição, erro := ioutil.ReadAll(r.Body)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnprocessableEntity, erro)
+		return
+	}
+
+	var usuario modelos.Usuario
+	if erro = json.Unmarshal(corpoRequisição, &usuario); erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositorioDeUsuario(db)
+	usuarioSalvoNoBanco, erro := repositorio.BuscarPorEmail(usuario.Email)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	if erro = seguranca.VerificarSenha(usuarioSalvoNoBanco.Senha, usuario.Senha); erro != nil {
+		respostas.Erro(w, http.StatusUnauthorized, erro)
+		return
+	}
+
+	w.Write([]byte("Você está logado! parabéns!"))
+}
--- api/src/repositorios/usuarios.go
+++ api/src/repositorios/usuarios.go
@@ -122,3 +122,22 @@ func (repositorio usuarios) Deletar(id uint64) error {
 
 	return nil
 }
+
+//BuscarPorEmail busca um usuário por email e o retorna o seu id e senha com hash
+func (repositorio usuarios) BuscarPorEmail(email string) (modelos.Usuario, error) {
+	linha, erro:= repositorio.db.Query("select id, senha from usuarios where email = ?", email)
+	if erro != nil {
+		return modelos.Usuario{}, erro
+	}
+	defer linha.Close()
+
+
+	var usuario modelos.Usuario
+	if linha.Next() {
+		if erro = linha.Scan(&usuario.ID, &usuario.Senha); erro != nil {
+			return modelos.Usuario{}, erro
+		}
+	}
+
+	return usuario, nil
+}
