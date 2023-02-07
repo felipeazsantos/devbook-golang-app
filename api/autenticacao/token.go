638661f97cce2365a28c6b3b2a06c13be764fcc6
Felipe Azevedo dos Santos <felipe.azevedo@cws.digital>
2023-02-07 12:00:33 -0300

    Validação do token

--- api/autenticacao/token.go
+++ api/autenticacao/token.go
@@ -2,8 +2,12 @@ package autenticacao
 
 import (
 	"DevBook/api/src/config"
+	"fmt"
 	jwt "github.com/dgrijalva/jwt-go"
+	"net/http"
+	"strings"
 	"time"
+	"errors"
 )
 
 //CriarToken gera uma string de autenticação com permissões para determinado usuário
@@ -14,4 +18,37 @@ func CriarToken(usuarioId uint64) (string, error) {
 	permissoes["usuarioId"] = usuarioId
 	token := jwt.NewWithClaims(jwt.SigningMethodES256, permissoes)
 	return token.SignedString(config.SecretKey) //secret
+}
+
+// ValidarToken verifica o token passado na requisição é valido
+func ValidarToken(r *http.Request) error {
+	tokenSring := extrairToken(r)
+	token, erro := jwt.Parse(tokenSring, retornarChaveDeVerificacao)
+	if erro != nil {
+		return erro
+	}
+
+	if _, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return nil
+	}
+
+	return errors.New("Token inválido")
+}
+
+func extrairToken(r *http.Request) string {
+	token := r.Header.Get("Authorization")
+
+	if len(strings.Split(token, " ")) == 2 {
+		return strings.Split(token, " ")[1]
+	}
+
+	return ""
+}
+
+func retornarChaveDeVerificacao(token *jwt.Token) (interface{}, error) {
+	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+		return nil, fmt.Errorf("Método de assinatura inesperado! %v", token.Header["alg"])
+	}
+
+	return config.SecretKey, nil
 }
