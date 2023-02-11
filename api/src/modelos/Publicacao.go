3a1cf4aae8622c772b2aef77c1f08fa51dfe4f53
Felipe Azevedo dos Santos <felipe.azevedo@cws.digital>
2023-02-11 13:24:14 -0300

    Validar e Formatar a criação da publicação

--- api/src/modelos/Publicacao.go
+++ api/src/modelos/Publicacao.go
@@ -1,6 +1,10 @@
 package modelos
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 //Publicacao representa uma publicação feita por um usuário
 type Publicacao struct {
@@ -12,4 +16,30 @@ type Publicacao struct {
 	Curtidas uint64  `json:"curtidas"`
 	CriadaEm time.Time `json:"criada_em,omitempty"`
 
+}
+// Preparar vai chamar os métodos para validar e formatar a publicação recebida
+func (publicacao *Publicacao) Preparar() error {
+	if erro := publicacao.validar(); erro != nil {
+		return erro
+	}
+
+	publicacao.formatar()
+	return nil
+}
+
+func (publicacao *Publicacao) validar() error {
+	if publicacao.Titulo == "" {
+		return errors.New("O título é obrigatório")
+	}
+
+	if publicacao.Conteudo == "" {
+		return errors.New("O conteúdo é obrigatório")
+	}
+
+	return nil
+}
+
+func (publicacao *Publicacao) formatar() {
+	publicacao.Titulo = strings.TrimSpace(publicacao.Titulo)
+	publicacao.Conteudo = strings.TrimSpace(publicacao.Conteudo)
 }
