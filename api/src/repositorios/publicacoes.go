1c4a9987afcbd45f6aa58db7b5070cfbe54d5b00
Felipe Azevedo dos Santos <felipe.azevedo@cws.digital>
2023-02-12 21:55:09 -0300

    Endpoint buscar publicações por usuário

--- api/src/repositorios/publicacoes.go
+++ api/src/repositorios/publicacoes.go
@@ -40,7 +40,7 @@ func (repositorio Publicacoes) Criar(publicacao modelos.Publicacao) (uint64, err
 func (repositorio Publicacoes) BuscarPorID(publicacaoID uint64) (modelos.Publicacao, error) {
 	linha, erro := repositorio.db.Query(
 		`select p,id, p.titulo, p.conteudo, p.autor_id, u.nick, p.curtidas, p.CriadoEm from usuarios
-				inner join publicacoes p on u.id = p.author_id 
+				inner join publicacoes p on u.id = p.autor_id 
 			    where p.id = ?`, publicacaoID)
 	if erro != nil {
 		return modelos.Publicacao{}, erro
@@ -69,8 +69,8 @@ func (repositorio Publicacoes) Buscar(usuarioID uint64) ([]modelos.Publicacao, e
 	linhas, erro := repositorio.db.Query(`
     select distinct p.*, u.nick from publicacoes p 
     inner join usuarios u on u.id = p.author_id
-    inner join seguidores s on p.author_id = s.usuario_id
-    where p.author_id = ? or s.seguidor_id = ?
+    inner join seguidores s on p.autor_id = s.usuario_id
+    where p.autor_id = ? or s.seguidor_id = ?
 	order by 1 desc
    `, usuarioID, usuarioID)
 	if erro != nil {
@@ -132,4 +132,41 @@ func (repositorio Publicacoes) Deletar(publicacaoID uint64) error {
 	}
 
 	return nil
+}
+
+// BuscarPorUsuario retorna as publicaões de um usuário específico
+func (repositorio Publicacoes) BuscarPorUsuario(usuarioId uint64) ([]modelos.Publicacao, error) {
+	linhas, erro := repositorio.db.Query(`
+	select p.*, u.nick from publicacoes p
+    inner join usuarios u on u.id = p.autor_id
+	where p.autor_id = ?
+    `, usuarioId)
+	if erro != nil {
+		return nil, erro
+	}
+	defer linhas.Close()
+
+	var publicacoes []modelos.Publicacao
+	for linhas.Next() {
+		var publicacao modelos.Publicacao
+
+		if erro = linhas.Scan(
+			&publicacao.ID,
+			&publicacao.Titulo,
+			&publicacao.Conteudo,
+			&publicacao.AutorID,
+			&publicacao.Curtidas,
+			&publicacao.CriadaEm,
+			&publicacao.AutorNick
+		); erro != nil {
+			return nil, erro
+		}
+
+		publicacoes = append(publicacoes, publicacao)
+	}
+
+	return publicacoes, nil
+
+
+
 }
