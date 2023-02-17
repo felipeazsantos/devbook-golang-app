03794e31bb320e7535ff8d6b55cce1453b2acc88
Felipe Azevedo dos Santos <felipe.azevedo@cws.digital>
2023-02-17 09:42:36 -0300

    Build modulo

--- api/sql/ambiente.sh
+++ api/sql/ambiente.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 docker volume create mysql-volume
 docker run --name container-mysql -p3306:3306 -v mysql-volume:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=root -d mysql/mysql-server:latest
 docker exec -it container-mysql bash
