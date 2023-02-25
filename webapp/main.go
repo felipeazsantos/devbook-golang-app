0110994ea41cf6a187824f9108ae5f4e7d2d3040
Felipe Azevedo dos Santos <felipe.azevedo@cws.digital>
2023-02-25 12:11:40 -0300

    Gerando hashKey e blockKey utilizando pacote securecookie

--- webapp/main.go
+++ webapp/main.go
@@ -10,6 +10,17 @@ import (
 	"net/http"
 )
 
+/*
+func init() {
+	hashKey := hex.EncodeToString(securecookie.GenerateRandomKey(16))
+	fmt.Println(hashKey)
+
+	blockKey := hex.EncodeToString(securecookie.GenerateRandomKey(16))
+	fmt.Println(blockKey)
+}
+*/
+
+
 func main() {
 	config.Carregar()
 	cookies.Configurar()
